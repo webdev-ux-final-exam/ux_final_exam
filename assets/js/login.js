ef654b868c2c548277cc808a4b68e574816d43b9
Steffen Andreas Kloster <steffenakloster@gmail.com>
2024-11-27 13:38:07 +0100

    Login page (#2)
    * feat: ApiHandler now returns objects, telling if the request was successful or not. if not, it includes HTTP status code, for proper error handling
    
    * feat:
    - makes sure the contents of the page always fills out the window
    - hover and disabled states on buttons
    
    * feat: added login page
    
    * fix: added userId to localStorage
    
    * fix: also store email

---
+++ assets/js/login.js
@@ -0,0 +1,35 @@
+import { toast } from "./toast.js";
+import ApiHandler from "./ApiHandler.js";
+
+const api = new ApiHandler("https://py-library-api-v2.server.steffen.codes");
+
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const submitButton = loginForm.querySelector("button[type=submit]");
+  const formData = new FormData(loginForm);
+  const data = Object.fromEntries(formData.entries());
+
+  submitButton.disabled = true;
+  const loginResult = await api.loginUser(data.email.trim(), data.password);
+  if (loginResult.success) {
+    localStorage.setItem("userId", data.email.trim());
+    localStorage.setItem("email", loginResult.data.user_id);
+    toast("Login successful, navigating you to the frontpage ..", "success");
+    setTimeout(() => (window.location.href = "/index.html"), 2000);
+  } else {
+    submitButton.disabled = false;
+    switch (loginResult.statusCode) {
+      case 400:
+        toast("Missing e-mail or password", "error");
+        break;
+      case 401:
+        toast("Invalid e-mail or password", "error");
+        break;
+      default:
+        toast("An error occurred", "error");
+    }
+  }
+});
