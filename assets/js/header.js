c41a6a40a0f5bfd75a81d167ad0391dfb5a9631b
sofiehyllen <sofiehyllen@hotmail.com>
2024-12-15 23:39:04 +0100

    header_adjustment

--- assets/js/header.js
+++ assets/js/header.js
@@ -19,13 +19,18 @@ const updateHeader = () => {
   const authButton = document.getElementById("auth-btn");
   const signupButton = document.getElementById("signup-btn")
   const logoButton = document.getElementById("logo-btn")
+  const userEmail  = localStorage.getItem("email")
 
   if (isUserLoggedIn()) {
     authButton.textContent = "Log Out";
     authButton.ariaLabel = "Log Out"
     authButton.onclick = logout;
     signupButton.style.display = "none"
-    logoButton.href = "/homepage.html"
+    if (userEmail === "admin.library@mail.com") {
+      logoButton.href = "/admin_homepage.html"
+    } else {
+      logoButton.href = "/homepage.html"
+    }
 
   } else {
     authButton.textContent = "Log In";
