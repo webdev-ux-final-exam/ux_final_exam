f0f11f71303ac6692bae35d65da348a2d4455ff0
steffenkloster <steffenakloster@gmail.com>
2025-02-21 00:28:04 +0100

    feat: responsive everywhere

--- assets/js/admin_tabs.js
+++ assets/js/admin_tabs.js
@@ -1,22 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  const tabContainer = document.querySelector(".tab");
   const tablinks = document.querySelectorAll(".tablinks");
   const tabContents = document.querySelectorAll(".tabcontent");
 
   // hide all tab content by default
-  tabContents.forEach(content => {
+  tabContents.forEach((content) => {
     content.style.display = "none";
   });
 
   // add event listener to each tab button
-  tablinks.forEach(tab => {
-    tab.addEventListener("click", function() {
+  tablinks.forEach((tab) => {
+    tab.addEventListener("click", function (e) {
+      if (e.target.id === "openMenu") {
+        tabContainer.classList.toggle("open");
+        return;
+      }
+
       // hide all content
-      tabContents.forEach(content => {
+      tabContents.forEach((content) => {
         content.style.display = "none";
       });
 
       // remove 'active' class from all buttons
-      tablinks.forEach(button => {
+      tablinks.forEach((button) => {
         button.classList.remove("active");
       });
 
@@ -25,6 +31,7 @@ document.addEventListener("DOMContentLoaded", function() {
       document.getElementById(tabId).style.display = "block";
 
       this.classList.add("active");
+      tabContainer.classList.remove("open");
     });
   });
   document.getElementById("AddBook").style.display = "block";
